cd23ade71b3fa77893a92077e15b24b69d0a3983
Alisha (Jungah Sohn) <aliciasohn815@gmail.com>
2022-05-26 11:17:38 +0200

    chat details markup

---
+++ pages/chats/[id].tsx
@@ -0,0 +1,31 @@
+import { NextPage } from "next";
+import React from "react";
+
+const ChatDetail: NextPage = () => {
+  return (
+    <div>
+      <div>
+        <div />
+        <div>Hi, how much are you selling them for?</div>
+      </div>
+      <div>
+        <div />
+        <div>I want $1000</div>
+      </div>
+      <div>
+        <div />
+        <div>Are you crazy??</div>
+      </div>
+      <div>
+        <div>
+          <input type="text" />
+          <div>
+            <span>&rarr;</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatDetail;
