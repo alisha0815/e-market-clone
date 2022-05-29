2d4769db3001548bbe7883b4fdb8ab21bba74c34
Alisha (Jungah Sohn) <aliciasohn815@gmail.com>
2022-05-29 23:55:43 +0200

    button component and type

---
+++ components/button.tsx
@@ -0,0 +1,26 @@
+import { cls } from "../libs/utils";
+
+interface ButtonProps {
+  large?: boolean;
+  text: string;
+  [key: string]: any;
+}
+
+export const Button = ({
+  large = false,
+  onClick,
+  text,
+  ...props
+}: ButtonProps) => {
+  return (
+    <button
+      {...props}
+      className={cls(
+        "mt-5 w-full bg-purple-700 rounded-md text-white py-3 text-lg hover:opacity-90 transition-opacity hover:scale-105",
+        large ? "py-3 text-base" : "py-2 text-sm "
+      )}
+    >
+      {text}
+    </button>
+  );
+};
